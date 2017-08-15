c13d5a9aa5a3f556fb800f2ffaeca49c220f444c
Glauter Santos <glauterlima@gmail.com>
2017-08-15 14:33:20 -0300

    ex 2.4 Exibindo os produtos na página - concluido

--- custom-express.js
+++ custom-express.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 module.exports = function () {
     const app = express();
+    app.use(express.static('./public'));
     app.set('view engine', 'ejs');
     require('./routes/produtos')(app);
     return app;
--- routes/produtos.js
+++ routes/produtos.js
@@ -1,6 +1,19 @@
 module.exports = function (app) {
     app.get('/produtos', (req, res) => {
-        res.render('produtos/lista');
-    });
+        //res.render('produtos/lista');
+        var mysql = require('mysql');
+        
+        var connection = mysql.createConnection({
+            host : 'localhost',
+            user: 'root',
+            password : '',
+            database : 'casadocodigo'
+        });
+        
+        connection.query('select * from livros' , function(err, result, fields) {
+            res.render('produtos/lista', {lista:result});
+        });
+        connection.end();
 
+});
 }
