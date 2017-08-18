d43a92eb007284519701ea9cb435e20c35b9b86e
Glauter Santos <glauterlima@gmail.com>
2017-08-18 15:35:50 -0300

    8.7 Notificando clientes sobre promocoes

---
+++ routes/promocoes.js
@@ -0,0 +1,18 @@
+module.exports = function(app) {
+    app.get('/promocoes/form', function (req,res) {
+        const connection = app.infra.connectionFactory();
+        const produtoDao = new app.infra.ProdutoDao(connection);
+        
+        produtoDao.lista(function (error,results) {
+            res.render('promocoes/form',{lista:results});    
+        });  
+        
+    });
+    
+    app.post('/promocoes', function(req,res) {
+        var promocao = req.body;
+        
+        app.get('io').emit('novaPromocao', promocao);
+        res.redirect('/promocoes/form');
+    });
+}
--- server.js
+++ server.js
@@ -1,6 +1,23 @@
-const ce = require('./custom-express');
+/*const ce = require('./custom-express');
 const app = ce();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.set('io',io);
 
 app.listen(3000, () => {
     console.log('Servidor Rodando');
+});
+*/
+var app = require('./custom-express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.set('io',io);
+
+var server = http.listen(3000, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    
+    console.log('Servidor executando em http://%s:%s', host, port);
 });
